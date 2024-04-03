759bc38936da935a32d9914d2f66f298b0c65e1a
KenshinKaraage <ani060901@ezweb.ne.jp>
2024-04-03 11:14:06 +0900

    the first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/saba.png">
+    <link rel="stylesheet" href="stylesheets/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@100..900&display=swap" rel="stylesheet">
+    <title>中西ブログ</title>
+</head>
+<body>
+    <?php include "templates/header.html"?>
+    <main>
+        <content>
+            <h1>ようこそ</h1>
+            <span>
+                自分の作った作品のポートフォリオのために作られたサイトです。<br>
+                どうぞ、ご高覧ください。
+            </span>
+        </content>
+        <?php include "templates/aside.html"?>
+    </main>
+    <?php include "templates/footer.html"?>
+</body>
+</html>
---
+++ works.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/saba.png">
+    <link rel="stylesheet" href="stylesheets/style.css">
+    <link rel="stylesheet" href="stylesheets/works.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@100..900&display=swap" rel="stylesheet">
+    <title>作品</title>
+</head>
+<body>
+    <?php include "templates/header.html"?>
+    <main>
+        <content>
+            <h1 id="pagetitle">作品</h1>
+            <div id="work-list">
+                <div class="work">
+                    <div class="work-content">
+                        <div class="work-image"><img src="images/articleImages/一人部屋３（日中）.jpg"></div>
+                        <div class="work-text">
+                            <div class="work-title">あの日の夕方</div>
+                            <div class="work-overview">露出狂を主人公としたハラハラ恋愛ゲーム</div>
+                        </div>
+                    </div>  
+                    <div class="work-article">
+                        <span>関連記事</span>
+                        <div class="article">
+                            <div class="article-image">
+                                <img src="images/articleImages/一人部屋３（日中）.jpg" >
+                            </div>
+                            <div class="article-text">
+                                <div class="article-title">あの日の夕方を作った</div>
+                                <div class="article-bottom">
+                                    <div class="article-date">2024/02/05</div>
+                                    <ul class="article-tags">
+                                        <li>ゲーム</li>
+                                        <li>自作</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="work">
+                    <div class="work-content">
+                        <div class="work-image"><img src="images/articleImages/一人部屋３（日中）.jpg"></div>
+                        <div class="work-text">
+                            <div class="work-title">あの日の夕方</div>
+                            <div class="work-overview">露出狂を主人公としたハラハラ恋愛ゲーム</div>
+                        </div>
+                    </div>  
+                </div>
+                <div class="work">
+                    <div class="work-content">
+                        <div class="work-image"><img src="images/articleImages/一人部屋３（日中）.jpg"></div>
+                        <div class="work-text">
+                            <div class="work-title">あの日の夕方</div>
+                            <div class="work-overview">露出狂を主人公としたハラハラ恋愛ゲーム</div>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </content>
+        <?php include "templates/aside.html"?>
+    </main>
+    <?php include "templates/footer.html"?>
+</body>
+</html>
